11271ebb41d96327b47676955a54895a84d215c6
Shantanu Shinde <shantanu.kshinde@gmail.com>
2025-02-07 15:25:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
-username = input("Enter your username: ")
-print(f"Hello, {username}!")
+import os 
+import subprocess
+import hashlib 
 
-num1 = int(input("Enter a number: "))
-num2 = int(input("Enter another number: "))
-print(f"The sum is: {num1 + num2}")
+filename = input("Enter a filename to read: ")
+with open(filename, 'r') as file:
+    data = file.read()
+
+password = 'secretpassword'
+
+command = input("Enter a shell command to execute: ")
+subprocess.run(command, shell=True)
+
+hashed_password = hashlib.md5(password.encode()).hexdigest()
+
+os.system('echo "This is a vulnerable file" > vulnerable_file.txt')
+
+username = input("Enter your username: ")
+if username == 'admin':
+    print("Welcome, admin!") 
